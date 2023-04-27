aaa1ab7a2dc6d5b9d69d4138b00cb224b1099fa8
yiulik <william2342009@gmail.com>
2023-04-27 00:08:32 -0600

    Changed the attribute name

--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -4,24 +4,24 @@ public class Prompt
     {
 
     }
-    public List<string> prompts = new List<string>();
+    public List<string> _prompts = new List<string>();
 
     public string GetPrompt()
     {
         // Create a list of prompts
-        prompts.Add("Who was the most interesting person I interacted with today?");
-        prompts.Add("What was the best part of my day?");
-        prompts.Add("How did I see the hand of the Lord in my life today?");
-        prompts.Add("What was the strongest emotion I felt today?");
-        prompts.Add("If I had one thing I could do over today, what would it be?");
-        prompts.Add("What big thing(s) happened today?");
-        prompts.Add("What is one thing that you are most grateful for today?");
-        prompts.Add("What is one thing that you spent a lot of time doing?");
+        _prompts.Add("Who was the most interesting person I interacted with today?");
+        _prompts.Add("What was the best part of my day?");
+        _prompts.Add("How did I see the hand of the Lord in my life today?");
+        _prompts.Add("What was the strongest emotion I felt today?");
+        _prompts.Add("If I had one thing I could do over today, what would it be?");
+        _prompts.Add("What big thing(s) happened today?");
+        _prompts.Add("What is one thing that you are most grateful for today?");
+        _prompts.Add("What is one thing that you spent a lot of time doing?");
 
-        // Randomize the promptss and ask for input
+        // Randomize the prompts and ask for input
         Random random = new Random();
-        int num = random.Next(prompts.Count);
+        int num = random.Next(_prompts.Count);
 
-        return prompts[num];
+        return _prompts[num];
     }
 }
--- prove/Develop02/Quote.cs
+++ prove/Develop02/Quote.cs
@@ -5,21 +5,21 @@ public class Quote
 
     }
 
-    public List<string> quotes = new List<string>();
+    public List<string> _quotes = new List<string>();
 
     public string GetQuote()
     {
-        quotes.Add("Persist in all things that are good and noble.");
-        quotes.Add("Are you a fool playing a genius, or a genius playing a fool?");
-        quotes.Add("Reading is like traveling, allowing you to exit your own life for a bit, and to come back with a renewed, even inspired, perspective.");
-        quotes.Add("Mastering others is strength. Mastering yourself is TRUE POWER.");
-        quotes.Add("Knowledge is of no value unless you put it into practice.");
-        quotes.Add("The reading of all good books is like conversation with the finest minds of past centuries.");
-        quotes.Add("Reading furnishes the mind only with materials of knowledge; it is thinking that makes what we read ours.");
+        _quotes.Add("Persist in all things that are good and noble.");
+        _quotes.Add("Are you a fool playing a genius, or a genius playing a fool?");
+        _quotes.Add("Reading is like traveling, allowing you to exit your own life for a bit, and to come back with a renewed, even inspired, perspective.");
+        _quotes.Add("Mastering others is strength. Mastering yourself is TRUE POWER.");
+        _quotes.Add("Knowledge is of no value unless you put it into practice.");
+        _quotes.Add("The reading of all good books is like conversation with the finest minds of past centuries.");
+        _quotes.Add("Reading furnishes the mind only with materials of knowledge; it is thinking that makes what we read ours.");
         
         Random random = new Random();
-        int num = random.Next(quotes.Count);
+        int num = random.Next(_quotes.Count);
 
-        return quotes[num];
+        return _quotes[num];
     }
 }
